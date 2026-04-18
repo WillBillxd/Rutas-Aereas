8f533a922f7fe139eb6b9eee493733f8e459e5a5
WillBillxd <edu16fer@gmail.com>
2026-04-18 02:37:25 -0500

    Update Funciones.h

--- Funciones.h
+++ Funciones.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
-
+#include <iomanip>
+using namespace std;
 void inicializarMatriz(int matriz[][100], int n) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
@@ -8,4 +9,30 @@ void inicializarMatriz(int matriz[][100], int n) {
         }
     }
 }
+void mostrarMatriz(int matriz[][100], int n, string ciudades[]) {
 
+    cout << "\nMatriz de conectividad:\n\n";
+
+   
+    cout << "      ";
+    for (int i = 0; i < n; i++) {
+        cout << ciudades[i] << " ";
+    }
+    cout << endl;
+
+  
+    for (int i = 0; i < n; i++) {
+        cout << ciudades[i] << " ";
+        for (int j = 0; j < n; j++) {
+            cout << matriz[i][j] << "       ";
+        }
+        cout << endl;
+    }
+}
+
+void cargarRutas(int matriz[][100]) {
+    matriz[0][1] = 1; 
+    matriz[1][2] = 1;
+    matriz[2][3] = 1; 
+   
+}
