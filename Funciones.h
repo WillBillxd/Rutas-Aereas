1278de355d7de0ff2d06d7b5d13d83272a7a34bb
WillBillxd <edu16fer@gmail.com>
2026-04-18 02:25:22 -0500

    Update Funciones.h

--- Funciones.h
+++ Funciones.h
@@ -1,1 +1,11 @@
+#pragma once
+#include <iostream>
+
+void inicializarMatriz(int matriz[][100], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            matriz[i][j] = 0;
+        }
+    }
+}
 
